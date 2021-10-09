bdae3b00072f276aee71ea92e25f88810837b7b9
JaviMera <javier_mera88@hotmail.com>
2021-10-09 09:07:34 -0500

    Begin adding code to FakeDataSource

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.udacity.project4.locationreminders.savereminder
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.runner.RunWith
@@ -11,6 +12,7 @@ class SaveReminderViewModelTest {
 
 
     //TODO: provide testing to the SaveReminderView and its live data objects
+    private lateinit var _saveReminderViewModel: SaveReminderViewModel
 
-
+    private lateinit var _fakeDataSource: FakeDataSource
 }
