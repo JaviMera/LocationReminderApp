caec3e1da5cc08ac1cda403aa157e245a42b556c
JaviMera <javier_mera88@hotmail.com>
2021-10-09 18:01:01 -0500

    Begin adding fragment tests.

---
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/FakeDataSource.kt
@@ -0,0 +1,33 @@
+package com.udacity.project4.locationreminders
+
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+
+class FakeDataSource(private val reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
+
+//    TODO: Create a fake data source to act as a double to the real data source
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        reminders?.let {
+            return Result.Success(ArrayList(it))
+        }
+
+        return Result.Error("Reminders not found")
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        reminders?.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        val reminder = reminders?.firstOrNull{it.id == id} ?: return Result.Error("Reminder not found")
+
+        return Result.Success(reminder)
+    }
+
+    override suspend fun deleteAllReminders() {
+        reminders?.clear()
+    }
+}
--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,30 +1,105 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
-import android.os.Bundle
+import android.Manifest
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.udacity.project4.R
+import androidx.test.rule.ActivityTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import androidx.test.rule.GrantPermissionRule
+import com.udacity.project4.locationreminders.FakeDataSource
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.data.local.RemindersDao
+import com.udacity.project4.locationreminders.data.local.RemindersDatabase
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+
+import org.junit.Rule
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.inject
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest : KoinTest {
 
+    private val _dataSource:ReminderDataSource by inject()
+    private lateinit var _database: RemindersDatabase
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Rule
+    val activityRule = ActivityTestRule(RemindersActivity::class.java)
+
+    @Rule
+    var runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    @Before
+    fun setupFragment() {
+        stopKoin()
+        startKoin {
+            androidContext(getApplicationContext())
+            modules(
+                module{
+                    viewModel {
+                        RemindersListViewModel(
+                            get(),
+                            get() as FakeDataSource
+                        )
+                    }
+                    single {
+                        Room.inMemoryDatabaseBuilder(
+                            getApplicationContext(),
+                            RemindersDatabase::class.java).allowMainThreadQueries().build() as RemindersDao
+                    }
+
+                    single {
+                        RemindersLocalRepository(get())
+                    }
+
+                    single {
+                        FakeDataSource() as ReminderDataSource
+                    }
+                }
+            )
+        }
+    }
+
+    @Test
+    fun saveReminder_displayedOnUi() = runBlockingTest {
+
+//        // Given
+//        val reminder = ReminderDTO("title", "description", "location", 10.0, 10.0)
+//        _dataSource.saveReminder(reminder)
+//
+//        // When
+//        launchFragmentInContainer<ReminderListFragment>()
+//
+//        // Then
+//        onView(withText("title")).check(matches(isDisplayed()))
+    }
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
